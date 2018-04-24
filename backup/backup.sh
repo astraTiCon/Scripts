127d3d13fc86a2303928e4f483455282a62418bd
mario <Mario.L.Geuenich@gmail.com>
2018-04-25 00:22:34 +0100

    barebone backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 
+#TODO in .config file:
+# maybe comments - #
+# ISSUES
+# no spaces destinationName in .config file allowed
+
+# $1 is the first argument
+
+# mkdir $1 # for later
+
+# copy all files and folders as specified in .config file to destination address, and
+# write --verbose output to log.txt
+myvar=$(cat .config)
+cp -rv $myvar ~/Desktop/ > log.txt
