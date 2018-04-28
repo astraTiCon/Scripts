6fee16ff03fcd970a4783838fb07cdce5d3c39f8
mario <Mario.L.Geuenich@gmail.com>
2018-04-28 16:51:31 +0100

    works with spaces in path

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-#TODO in .config file:
-# maybe comments - #
-# ISSUES
-# no spaces destinationName in .config file allowed
+#TODO
+# in .config file: maybe comments - #
+# could store files in compressed archive (tar)
+# check number of args
 
-# mkdir $1 # for later
-
-# copy all files and folders specified in .config file to destination address (given by arg),
-# and write --verbose output to log.txt
-while read line; do
-    cp -rv $line ~/Desktop/ > log.txt
+# copy everyting specified in .config to destination (given by arg, $1), log output
+echo "" > log.txt # overwrite previous log content
+while read -r line; do
+    cp -rv "$line" $1 >> log.txt # $line enclosed in quotes is necessary -> preserves
+    # the literal value of all characters within the quotes, with the exception of $, `, \, and, 
+    # when history expansion is enabled, !
 done < ".config"
